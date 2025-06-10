fb5a629a4c9fe4b257d9d6ff8d2332d41dd938ef
kael0595 <changgyu549@gmail.com>
2025-06-11 03:08:46 +0900

    spring security 연동을 위한 MemberDetailService 작성

--- src/main/java/com/project/P_list/member/repository/MemberRepository.java
+++ src/main/java/com/project/P_list/member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.project.P_list.member.repository;
 import com.project.P_list.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
 }
---
+++ src/main/java/com/project/P_list/member/service/MemberDetailService.java
@@ -0,0 +1,30 @@
+package com.project.P_list.member.service;
+
+import com.project.P_list.member.entity.Member;
+import com.project.P_list.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return new User(
+                member.getUsername(), member.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_" + member.getGrade().name()))
+        );
+    }
+}
