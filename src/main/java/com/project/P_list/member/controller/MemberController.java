e75a69358e79e3fecd07dceb826b87b15fd65a86
kael0595 <changgyu549@gmail.com>
2025-06-11 02:53:27 +0900

    회원가입 및 권한부여 작성

--- src/main/java/com/project/P_list/member/controller/MemberController.java
+++ src/main/java/com/project/P_list/member/controller/MemberController.java
@@ -1,9 +1,13 @@
 package com.project.P_list.member.controller;
 
+import com.project.P_list.member.dto.MemberDto;
 import com.project.P_list.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,5 +20,23 @@ public class MemberController {
     @GetMapping("/join")
     public String joinForm() {
         return "/member/join";
+    }
+
+    @PostMapping("/join")
+    public String join(@Valid MemberDto memberDto,
+                       BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "member/join";
+        }
+
+        if (!memberDto.getPassword().equals(memberDto.getPasswordCnf())) {
+            return "member/join";
+        }
+
+        memberService.join(memberDto);
+
+        return "/member/login";
+
     }
 }
--- src/main/java/com/project/P_list/member/service/MemberService.java
+++ src/main/java/com/project/P_list/member/service/MemberService.java
@@ -1,9 +1,41 @@
 package com.project.P_list.member.service;
 
+import com.project.P_list.member.dto.MemberDto;
+import com.project.P_list.member.entity.Member;
+import com.project.P_list.member.enums.Grade;
+import com.project.P_list.member.repository.MemberRepository;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public void join(MemberDto memberDto) {
+
+        Grade grade = determineGrade(memberDto.getUsername());
+
+        Member member = Member.builder()
+                .username(memberDto.getUsername())
+                .password(passwordEncoder.encode(memberDto.getPassword()))
+                .email(memberDto.getEmail())
+                .nickname(memberDto.getNickname())
+                .addr1(memberDto.getAddr1())
+                .addr2(memberDto.getAddr2())
+                .grade(grade)
+                .build();
+
+        memberRepository.save(member);
+
+    }
+
+    private Grade determineGrade(String username) {
+        return username.startsWith("admin") ? Grade.ADMIN : Grade.USER;
+    }
 }
