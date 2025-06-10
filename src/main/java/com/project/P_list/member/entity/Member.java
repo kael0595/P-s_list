0a7e1657cf29a39538f08fe299a24429008c8fbd
kael0595 <changgyu549@gmail.com>
2025-06-11 01:29:28 +0900

    member Entity에 validation 추가

--- src/main/java/com/project/P_list/member/entity/Member.java
+++ src/main/java/com/project/P_list/member/entity/Member.java
@@ -1,7 +1,10 @@
 package com.project.P_list.member.entity;
 
 import com.project.P_list.base.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,14 +17,26 @@ import lombok.NoArgsConstructor;
 @Data
 public class Member extends BaseEntity {
 
+    @NotNull
+    @Column(nullable = false)
+    @Size(max = 20)
     private String username;
 
+    @NotNull
+    @Column(nullable = false)
+    @Size(min = 8)
     private String password;
 
+    @NotNull
+    @Column(nullable = false)
     private String email;
 
+    @NotNull
+    @Column(nullable = false)
     private String nickname;
 
+    @NotNull
+    @Column(nullable = false)
     private String addr1;
 
     private String addr2;
