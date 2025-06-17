0a9b56ddf9c3be9f5fd262d9e286cdb3a089f920
kael0595 <changgyu549@gmail.com>
2025-06-17 16:38:24 +0900

    spring security를 사용하여 인증/인가를 하기위해 SecurityUser 작성

---
+++ src/main/java/com/project/P_list/base/security/SecurityUser.java
@@ -0,0 +1,54 @@
+package com.project.P_list.base.security;
+
+import com.project.P_list.member.entity.Member;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class SecurityUser implements UserDetails {
+
+    private final Member member;
+
+    public SecurityUser(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "ROLE_USER");
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
