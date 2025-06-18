8e7128e375322d26489544911f5b9ad515dbf71d
kael0595 <changgyu549@gmail.com>
2025-06-18 18:38:20 +0900

    페이징을 위한 countByKeyword 메소드 작성

--- src/main/java/com/project/P_list/board/repository/BoardRepository.java
+++ src/main/java/com/project/P_list/board/repository/BoardRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
     Page<Board> findAll(Specification<Board> spec, Pageable pageable);
+
+    @Query("SELECT COUNT(b) FROM Board b WHERE b.title LIKE %:kw% OR b.content LIKE %:kw% OR b.author.username LIKE %:kw%")
+    int countByKeyword(@Param("kw") String kw);
 }
