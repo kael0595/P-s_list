ca4595ee3196e4586e9d30ec8060ec3018bcfd56
kael0595 <changgyu549@gmail.com>
2025-08-30 23:45:38 +0900

    사용자 권한 수정 추가

--- src/main/java/com/project/P_list/admin/controller/AdminController.java
+++ src/main/java/com/project/P_list/admin/controller/AdminController.java
@@ -1,12 +1,18 @@
 package com.project.P_list.admin.controller;
 
+import com.project.P_list.base.security.SecurityUser;
+import com.project.P_list.member.dto.MemberDto;
 import com.project.P_list.member.entity.Member;
+import com.project.P_list.member.enums.Grade;
 import com.project.P_list.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +31,23 @@ public class AdminController {
         model.addAttribute("memberList", memberList);
 
         return "admin/memberList";
+    }
+
+    @PostMapping("/updateAuth")
+    @ResponseBody
+    public ResponseEntity<?> updateAuth(@AuthenticationPrincipal SecurityUser securityUser,
+                                        @RequestBody MemberDto memberDto) {
+
+        Member loginMember = memberService.getMember(securityUser.getUsername());
+
+        Member targetMember = memberService.getMember(memberDto.getUsername());
+
+        if (loginMember.getGrade() != Grade.ADMIN) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("수정 권한이 없습니다.");
+        }
+
+        memberService.changAuth(targetMember);
+
+        return ResponseEntity.ok().build();
     }
 }
